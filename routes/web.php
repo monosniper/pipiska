21ec7eed33772f7db8568072ffbebbb7f007e36a
Radzi7 <vanteateam@gmail.com>
2024-07-09 15:52:25 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('translate', Translate::class);
 Route::get('/', Translate::class)->domain('translate.'.config('app.url'));
+Route::get('test', function () {
+    dd((new LaravelLang\Translator\Services\Translate())->viaGoogle('Привет', 'uz', 'ru'));
+});
