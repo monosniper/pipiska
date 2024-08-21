b75fa3eae1d3a1b3060fbd5d13701a8b8d5b2bc4
ravilto <vanteateam@gmail.com>
2024-08-21 18:30:43 +0500

    upd

--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -1,9 +1,7 @@
 <?php
 
-return
-
-[
-	'failed' => 'These credentials do not match our records.',
+return [
+	'failed' => 'пока',
 	'password' => 'The provided password is incorrect.',
 	'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
---
+++ lang/ru/nested.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'first' => [
+        'hello' => 'dd',
+        'hegfhfghllo' => 'dd',
+        'second' => [
+            'hello2' => 'dd2',
+            'thirst' => [
+                'hello3' => 'dd3',
+            ]
+        ],
+        'sfhfgh' => 'dd',
+    ],
+    'thirst' => [
+        'hello3' => 'dd3',
+    ]
+];
